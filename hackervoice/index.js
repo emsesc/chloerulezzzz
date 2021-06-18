ef521041c73e232488028784177c44f6428925d5
emsesc <emseschen@gmail.com>
2021-06-18 13:58:53 -0400

    new hackervoice changes

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -2,9 +2,15 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     var password = req.query.password
+    var output = ""
+    if (password == "letmein") {
+        output = "Access granted."
+    } else {
+        output = "Access denied."
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: output
     };
 }
