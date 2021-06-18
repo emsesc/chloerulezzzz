4cec3bbeea8d47ee1aa3cce6b4f7670fae6dbb0a
emsesc <emseschen@gmail.com>
2021-06-18 14:23:57 -0400

    twocatz v 2

--- TwoCatz/index.js
+++ TwoCatz/index.js
@@ -1,23 +1,38 @@
 const fetch = require('node-fetch')
+// npm install node-fetch
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    let resp = await fetch("https://cataas.com/cat/says/serverless", {
-        method: 'GET'
-    });
-    
-    let data = await resp.arrayBuffer()
+    async function getCatPic() {
+        let resp = await fetch("https://cataas.com/cat/cute/says/Bitcamp", {
+            method: 'GET'
+        });
+        
+        let data = await resp.arrayBuffer()
+        context.log(data)
+        data = Buffer.from(data).toString('base64')
+        return data
+    }
 
-    base64data = Buffer.from(data).toString('base64')
-    //put what you want to turn into base64 inside "originaldata"
-    //"originaldata" will be encoded in base64.
+    function getNames() {
+        var names = ["Shreya", "Emily", "Fifi", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"]
+        var random_value = Math.floor(names.length * Math.random())
+        var resultname = names[random_value]
+        return resultname
+    }
 
-    // we need to receive it as a buffer since this is an image we are receiving from the API
-    // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
-    
+    let firstcat = await getCatPic()
+    let secondcat = await getCatPic()
+    let name1 = getNames()
+    let name2 = getNames()
+ 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: base64data
+        body: {
+            cat1: firstcat,
+            cat2: secondcat,
+            names: [name1, name2]
+        }
     };
 }
